070d0356f7bc338ea5998b6b8d5a941d0161c8ed
dungasamsungo <sampsonngobi@gmail.com>
2024-03-21 19:57:34 -0300

    added websites to the the links

--- script.js
+++ script.js
@@ -16,4 +16,19 @@ function toggleMode(){
     const html = document.documentElement
     
     html.classList.toggle("light")
+
+    //select the image
+    const img = document.querySelector("#profile img")
+
+    //substitute image 
+    if(html.classList.contains('light')) {
+
+    //if  it is light, select the ligh image
+        img.setAttribute("src", 'images/Profile.png')
+    //if not use the normal image 
+    }else{
+        img.setAttribute("src", "images/profile-light.png")   
+    }
+
+
 }
